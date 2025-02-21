d660601d0401243e353265e08c2d5d8e069d769d
Anagh Mehran <anagh07@gmail.com>
2025-02-21 02:09:21 -0800

    2620 - Counter

---
+++ closures/counter.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+const createCounter = (n) => {
+  return () => {
+    return n++;
+  };
+};
+
+const counter = createCounter(10);
+console.log(counter()); // 10
+console.log(counter()); // 11
+console.log(counter()); // 12
