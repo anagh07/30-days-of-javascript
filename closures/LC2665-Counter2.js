cc2e2cdea549640b746effe1037570e266d9a6ae
Anagh Mehran <anagh07@gmail.com>
2025-02-21 18:46:55 -0800

    LC2665 - Counter II using class

--- closures/LC2665-Counter2.js
+++ closures/LC2665-Counter2.js
@@ -21,3 +21,35 @@ const counter = createCounter(5)
 console.log(counter.increment()); // 6
 console.log(counter.reset()); // 5
 console.log(counter.decrement()); // 4
+
+
+class Counter {
+  #init;
+  #count;
+
+  constructor(init) {
+    this.#init = init;
+    this.#count = init;
+  }
+
+  reset() {
+    this.#count = this.#init;
+    return this.#count;
+  }
+
+  increment() {
+    return ++this.#count;
+  }
+
+  decrement() {
+    return --this.#count;
+  }
+}
+
+const classCounter = new Counter(5);
+console.log("\nClass Based Counter")
+console.log(classCounter.increment()) // 6
+console.log(classCounter.decrement()) // 5
+console.log(classCounter.decrement()) // 4
+console.log(classCounter.decrement()) // 3
+console.log(classCounter.reset())     // 5
