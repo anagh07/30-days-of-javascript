a8dac7a0312d757be0b028a99c410581e842971f
Anagh Mehran <anagh07@gmail.com>
2025-02-23 17:18:34 -0800

    LC2626 - Array Reduce Transform using for-loop and arr.reduce - Tests added

--- array-transformations/LC2626-ArrayReduceTransform.js
+++ array-transformations/LC2626-ArrayReduceTransform.js
@@ -5,5 +5,16 @@
  * @return {number}
  */
 export const reduce = (nums, fn, init) => {
-    return 0;
-}
+  let accumulator = init == null ? 0 : init;
+  for (let i = 0; i < nums.length; i++) {
+    accumulator = fn(accumulator, nums[i]);
+  }
+  return accumulator;
+};
+
+/**
+ * Built-in array.reduce
+ */
+export const arrReduce = (nums, fn, init = 0) => {
+  return nums.reduce(fn, init);
+};
--- array-transformations/LC2626-ArrayReduceTransform.test.js
+++ array-transformations/LC2626-ArrayReduceTransform.test.js
@@ -1,5 +1,5 @@
 import { assertEquals } from "jsr:@std/assert";
-import { reduce } from "./LC2626-ArrayReduceTransform.js";
+import { arrReduce, reduce } from "./LC2626-ArrayReduceTransform.js";
 
 const numbers = [1, 2, 3, 4];
 
@@ -18,5 +18,26 @@ Deno.test("reduce: zero", () => {
 Deno.test("reduce: product init", () => {
     const want = 240;
     const got = reduce(numbers, (accum, curr) => accum * curr, 10);
+    assertEquals(got, want);
+});
+
+/**
+ * Built-in arr.reduce method
+ */
+Deno.test("array.reduce: sum", () => {
+    const want = 10;
+    const got = arrReduce(numbers, (accum, curr) => accum + curr);
+    assertEquals(got, want);
+});
+
+Deno.test("array.reduce: zero", () => {
+    const want = 0;
+    const got = arrReduce(numbers, (accum, curr) => 0);
+    assertEquals(got, want);
+});
+
+Deno.test("array.reduce: product init", () => {
+    const want = 240;
+    const got = arrReduce(numbers, (accum, curr) => accum * curr, 10);
     assertEquals(got, want);
 });
