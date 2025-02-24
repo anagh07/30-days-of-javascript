55007c381034dbf014456e10c16004478e3ddd78
Anagh Mehran <anagh07@gmail.com>
2025-02-24 07:15:23 -0800

    LC2629 - Function Composition - tests and debugging included

--- function-transformations/LC2629-FunctionComposition.js
+++ function-transformations/LC2629-FunctionComposition.js
@@ -3,14 +3,34 @@
  * @return {Function}
  */
 export const compose = (functions) => {
-    return (x) => {
-        let accum = x;
-        for (let i = functions.length - 1; i >= 0; i--) {
-            accum = functions[i](accum);
-        }
-        return accum;
-    };
+  return (x) => {
+    let accum = x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      accum = functions[i](accum);
+    }
+    return accum;
+  };
 };
 
 const fn = compose([(x) => x + 1, (x) => 2 * x]);
 console.log(fn(4)); // 9
+
+/**
+ * Array reduce-right function
+ */
+export const composeReduceRight = (functions) => {
+  if (functions.length === 0) {
+    return (x) => x;
+  }
+
+  return functions.reduceRight((accum, currVal) => {
+    return (x) => {
+      return currVal(accum(x));
+    };
+  });
+};
+
+const fn2 = composeReduceRight([(x) => x + 1, (x) => 2 * x]);
+const fn3 = composeReduceRight([(x) => x + 1, (x) => x * x, (x) => 2 * x]);
+console.log(fn2(4)); // 9
+console.log(fn3(4)); // 9
--- function-transformations/LC2629-FunctionComposition.test.js
+++ function-transformations/LC2629-FunctionComposition.test.js
@@ -1,37 +1,43 @@
-import { compose } from "./LC2629-FunctionComposition.js";
+import { compose, composeReduceRight } from "./LC2629-FunctionComposition.js";
 import { assertEquals } from "jsr:@std/assert/equals";
 
-Deno.test(
-    "Case 1: Empty func array",
-    () => {
-        const want = 4;
-        const got = compose([])(4);
-        assertEquals(got, want);
-    },
-);
+Deno.test("Case 1: Empty func array", () => {
+  const want = 4;
+  const got = compose([])(4);
+  assertEquals(got, want);
+});
 
-Deno.test(
-    "Case 2: ",
-    () => {
-        const want = 65;
-        const got = compose([
-            (x) => x + 1,
-            (x) => x * x,
-            (x) => 2 * x,
-        ])(4);
-        assertEquals(got, want);
-    },
-);
+Deno.test("Case 2: ", () => {
+  const want = 65;
+  const got = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x])(4);
+  assertEquals(got, want);
+});
 
-Deno.test(
-    "Case 3: ",
-    () => {
-        const want = 1000;
-        const got = compose([
-            (x) => 10 * x,
-            (x) => 10 * x,
-            (x) => 10 * x,
-        ])(1);
-        assertEquals(got, want);
-    },
-);
+Deno.test("Case 3: ", () => {
+  const want = 1000;
+  const got = compose([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x])(1);
+  assertEquals(got, want);
+});
+
+/**
+ * Array reduce-right method
+ */
+Deno.test("Case 1: Empty func array", () => {
+  const want = 4;
+  const got = composeReduceRight([])(4);
+  assertEquals(got, want);
+});
+
+Deno.test("Case 2: ", () => {
+  const want = 65;
+  const got = composeReduceRight([(x) => x + 1, (x) => x * x, (x) => 2 * x])(4);
+  assertEquals(got, want);
+});
+
+Deno.test("Case 3: ", () => {
+  const want = 1000;
+  const got = composeReduceRight([(x) => 10 * x, (x) => 10 * x, (x) => 10 * x])(
+    1,
+  );
+  assertEquals(got, want);
+});
