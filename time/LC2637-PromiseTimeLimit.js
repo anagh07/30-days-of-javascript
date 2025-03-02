2ceb648e496432752ba8b952a4a45abd7bac946e
Anagh Mehran <anagh07@gmail.com>
2025-03-01 22:46:21 -0800

    LC2637 - Promise Time Limit with await and Promise.race()

--- time/LC2637-PromiseTimeLimit.js
+++ time/LC2637-PromiseTimeLimit.js
@@ -21,3 +21,27 @@ var timeLimit = function (fn, t) {
 
 const limited = timeLimit((t) => new Promise((res) => setTimeout(res, t)), 100);
 limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
+
+/**
+ * Using Promise.race()
+ */
+const timeLimitRace = (fn, t) => {
+  return async (...args) => {
+    const promiseResolve = fn(...args);
+    const promiseReject = new Promise((_, reject) => {
+      setTimeout(() => reject("Time Limit Exceeded"), t);
+    });
+
+    try {
+      return await Promise.race([promiseResolve, promiseReject]);
+    } catch (error) {
+      throw error;
+    }
+  };
+};
+
+const limitedRace = timeLimitRace(
+  (t) => new Promise((res) => setTimeout(res, t)),
+  100,
+);
+limitedRace(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
